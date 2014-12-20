bf1bd001a08a748a9e83e8221247869fcc4f736d
Jon Skulski <jskulski@gmail.com>
2014-12-20 00:02:09 -0500

    Refactor tests to match requirements

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -5,35 +5,25 @@ describe "Fizz Buzz Mapper" do
 
   Given(:mapper) { Mapper.new }
 
-  context "mapper yields" do 
+  context "mapper yields" do
 
-    it "1 yields 1" do 
+    it "1 yields 1" do
       result = mapper.map(2)
       expect(result).to eq(2)
     end
 
-    it "2 yields 2" do 
+    it "2 yields 2" do
       result = mapper.map(2)
       expect(result).to eq(2)
     end
 
-    it "3 yields fizz" do
-      result = mapper.map(3)
+    it "multiples of three yield fizz" do
+      result = mapper.map(9)
       expect(result).to eq("fizz")
     end
 
-    it "5 yields buzz" do
-      result = mapper.map(5)
-      expect(result).to eq("buzz")
-    end
-
-    it "6 yields fizz" do
-      result = mapper.map(6)
-      expect(result).to eq("fizz")
-    end
-
-    it "10 yields buzz" do
-      result = mapper.map(10)
+    it "multiples of five yield buzz" do
+      result = mapper.map(50)
       expect(result).to eq("buzz")
     end
 
